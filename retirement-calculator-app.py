5145c42e283b281bc35edf80154eb4d2ac1e723e
ezekielstratton <59847127+ezekielstratton@users.noreply.github.com>
2024-09-01 12:50:55 -0700

    Update retirement-calculator-app.py

--- retirement-calculator-app.py
+++ retirement-calculator-app.py
@@ -5,42 +5,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib.ticker import FuncFormatter
 
-# Inject custom CSS
-st.markdown("""
-<style>
-/* General styles for better readability */
-body {
-    color: #333333; /* A neutral dark color for text */
-    background-color: #f5f5f5; /* Light background */
-}
-
-/* Adjust text for dark mode */
-@media (prefers-color-scheme: dark) {
-    body {
-        color: #f5f5f5; /* Light text color for dark mode */
-        background-color: #333333; /* Dark background */
-    }
-}
-
-/* Styling for Streamlit components */
-.stButton>button {
-    background-color: #4CAF50; /* Green background for buttons */
-    color: white; /* White text */
-}
-
-/* Dark mode button styling */
-@media (prefers-color-scheme: dark) {
-    .stButton>button {
-        background-color: #1a73e8; /* Blue background for dark mode */
-        color: white; /* White text */
-    }
-}
-
-/* Styling other elements as needed... */
-</style>
-""", unsafe_allow_html=True)
-
-
 # Tax configuration constants
 TAX_BRACKETS = {
     'income': [(0, 11000), (11001, 44725), (44726, 95375), (95376, 182100), (182101, 231250), (231251, 578125), (578126, float('inf'))],
