47d4c17af6a93e12373c4ea03bb899c510e1f561
ezekielstratton <59847127+ezekielstratton@users.noreply.github.com>
2025-05-23 15:17:19 -0700

    removed dollar sign from age

--- retirement-calculator-app.py
+++ retirement-calculator-app.py
@@ -162,6 +162,7 @@ ax2.legend()
 st.pyplot(fig2)
 
 # Detailed Data Table
+df = df.set_index('Age')
 st.subheader("Detailed Financial Projections")
 st.dataframe(df[['Age', 'Salary', 'After-Tax Income', 'Spending', 'Retirement Contribution', 'Net Worth', 'Investment Income']]
              .style.format('${:,.0f}'))
